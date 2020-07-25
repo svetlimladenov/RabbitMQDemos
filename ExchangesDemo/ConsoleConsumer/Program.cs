2b34ec24def5e0c5edeba03a581b232800955778
Svetlin <svetlimladenov@gmail.com>
2020-07-25 17:23:12 +0300

    add exchanges sln infrastructure

---
+++ ExchangesDemo/ConsoleConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
