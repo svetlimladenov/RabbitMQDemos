24eadfa4fcc63f58ac5beb492fd8edfeb775e027
Svetlin <svetlimladenov@gmail.com>
2020-07-31 21:49:34 +0300

    Add Post Microservice Runner And Service

---
+++ ExchangesDemo/Microservices/Post/PostMicroservice.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microservices.Post.PostMicroservice.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
